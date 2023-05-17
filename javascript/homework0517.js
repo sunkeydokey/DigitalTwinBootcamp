0b68f802af795cc3792100fccbf8c9b47db15bd0
sunkeydokey <sunkeydokey@gmail.com>
2023-05-17 16:36:57 +0900

    0517 js 과제

---
+++ javascript/homework0517.js
@@ -0,0 +1,27 @@
+// 1
+const HIGH_TEMPERATURES = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80,
+};
+
+// Only change code below this line
+
+const { today } = HIGH_TEMPERATURES;
+const { tomorrow } = HIGH_TEMPERATURES;
+
+// Only change code above this line
+
+// 2
+const stats = {
+  max: 56.78,
+  standard_deviation: 4.34,
+  median: 34.54,
+  mode: 23.87,
+  min: -0.75,
+  average: 35.85,
+};
+
+// Only change code below this line
+const half = ({ max, min }) => (max + min) / 2.0;
+// Only change code above this line
